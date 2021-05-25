e9fa37381f0966266b7c67f7ae6b37858d8a997d
emireken <emirgokberk.eken@gmail.com>
2021-05-24 23:20:09 -0400

    v1

---
+++ src/config.js
@@ -0,0 +1,33 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken:
+    "Z2hwX2s5d3o2MjBhVk9ZS1J6NUpJTFFkblladW9RSlI1ODRYVVZQcQ==",
+
+  // Your Github UserName
+  githubUserName: "emireken",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "react-blog-github",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: "Iv1.1acc9ef037bbf0e9",
+  OAuthSecret: "f4929c4b06457d9253a0accd97dea80f40375300",
+
+  // Your Personal Blog Title
+  title: "Emir Eken",
+
+  // Your Personal Blog Subtitle
+  subtitle: "Software Developer",
+
+  // Header customization
+  header: {
+    backgroundColor: "#f1f6f8", // can be a CSS gradient
+    backgroundColorDark: "#4C566A",
+    titleColor: "#ff5252",
+    titleColorDark: "#16a085",
+    subtitleColor: "#37474f",
+    subtitleColorDark: "#D8DEE9",
+  },
+};
