77d83c80149c577a2233aede8b1ff2eaf005b91e
emireken <emirgokberk.eken@gmail.com>
2021-05-27 12:16:04 -0400

    Updates

---
+++ precache-manifest.dea83e892fc79f1d2da529f0694d60db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6fc3d6077b888e9636a176152cf265",
+    "url": "/egek-blog-app/index.html"
+  },
+  {
+    "revision": "2141f5def8ec505dad91",
+    "url": "/egek-blog-app/static/css/main.1b920327.chunk.css"
+  },
+  {
+    "revision": "ba5ae5e87f09425a9bf1",
+    "url": "/egek-blog-app/static/js/2.4342b1d5.chunk.js"
+  },
+  {
+    "revision": "189e23ef3562d267680aa9086e84695d",
+    "url": "/egek-blog-app/static/js/2.4342b1d5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2141f5def8ec505dad91",
+    "url": "/egek-blog-app/static/js/main.86f3ef05.chunk.js"
+  },
+  {
+    "revision": "f0b8f01273f7a68a0112",
+    "url": "/egek-blog-app/static/js/runtime-main.9729be4f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/egek-blog-app/precache-manifest.dea83e892fc79f1d2da529f0694d60db.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/egek-blog-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
